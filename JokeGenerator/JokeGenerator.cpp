564999807711d9797e3c0f3d79c798abd42662f8
bradym08 <bradymcneely062201@gmail.com>
2021-07-31 17:43:47 -0400

    Add project files.

---
+++ JokeGenerator/JokeGenerator.cpp
@@ -0,0 +1,17 @@
+/* * * * * * * * * * * * * * * * * * *
+ * Collin Gee and Brady McNeely
+ *
+ * Project 3: Reddit Joke Generator
+ * * * * * * * * * * * * * * * * * * */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello World!" << endl;
+
+
+	return 0;
+}
