881239886bc221d93e02abcf22e539f145ca6296
collingee <collingee@ufl.edu>
2021-08-01 00:23:27 -0400

    Added more code

--- JokeGenerator/JokeGenerator.cpp
+++ JokeGenerator/JokeGenerator.cpp
@@ -4,7 +4,8 @@
  * Project 3: Reddit Joke Generator
  * * * * * * * * * * * * * * * * * * */
 
-#include "jsoncpp-master\dist\json\json.h"
+//#include "jsoncpp-master/dist/json/json.h" <- didn't exist
+#include "jsoncpp-master/include/json/json.h"
 #include <iostream>
 #include <fstream>
 
@@ -14,16 +15,42 @@ class Joke {
 private:
 	string title, body, id;
 	int score;
+	int titleSize;
+	int bodySize;
 
+	void setTitleSize() {
+		this->titleSize = title.size();
+	}
+	void setBodySize() {
+		this->bodySize = body.size();
+	}
 public:
-	Joke();
-	Joke(string newId, string newTitle, string newBody, int newScore);
+	
+
+
+	Joke() {
+		// Shouldn't ever need this
+		// Setting the 
+		cout << "We are calling the default joke constructor\nThis is an issue\n";
+		this->id = "DEFAULT JOKE CONSTRUCTOR";
+		this->title = "DEFAULT JOKE CONSTRUCTOR";
+		this->body = "DEFAULT JOKE CONSTRUCTOR";
+		this->score = 999999999;
+	}
+	Joke(string newId, string newTitle, string newBody, int newScore) {
+		this->id = newId;
+		this->title = newTitle;
+		this->body = newBody;
+		this->score = newScore;
+	}
 };
 
+// Implementation of Shell Sort
 void shellSort(Joke jokes[]) {
 
 }
 
+// Implementation of Shell Sort
 void mergeSort(Joke jokes[]) {
 
 }
